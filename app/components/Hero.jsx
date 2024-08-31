3666264e3c9153096919bb8653a3d4930b7f16bc
gmells <victormells30@gmail.com>
2024-08-31 19:23:30 +0100

    Add input component to Hero file and also a search component from lucide-react

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex items-center flex-col justify-center pt-14 pb-7">
+    <div className="flex items-center gap-3 flex-col justify-center pt-14 pb-7">
       <h2 className="font-bold text-[46px] text-center">
         Find Home
         <span className="text-primary">Service/Repair</span>
@@ -12,7 +15,12 @@ const Hero = () => {
         Explore Best Home Service & Repair Near You
       </h2>
 
-      <div></div>
+      <div className="mt-4 flex gap-4 items-center">
+        <Input placeholder="Search" className="rounded-full md:w-[350px]" />
+        <Button className="rounded-full h-[46px]">
+          <Search className="h-5 w-4" />
+        </Button>
+      </div>
     </div>
   );
 };
