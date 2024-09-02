cb01c645278a024d6bac83d8185c0fe3ccabed9b
gmells <victormells30@gmail.com>
2024-09-02 12:36:14 +0100

    Add CategoryList component, GlobalApi file and  useEffect from react to page file

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./components/Hero";
+import CategoryList from "./components/CategoryList";
+import GlobalApi from "./services/GlobalApi";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then((resp) => {
+      console.log(resp);
+    });
+  };
   return (
     <div>
       <Hero />
+
+      <CategoryList />
     </div>
   );
 }
