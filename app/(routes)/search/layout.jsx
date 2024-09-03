1c39c433c8c6dbca9e7e47f2075dd95afd12acad
gmells <victormells30@gmail.com>
2024-09-04 00:18:06 +0100

    add layout for Side Category Nav Bar

---
+++ app/(routes)/search/layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CategorySidebar from "./components/CategorySidebar";
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-4 mt-8">
+        <div className="hidden md:block">
+          <CategorySidebar />
+          {/*Side Category Nav Bar*/}
+        </div>
+        <div className="md:col-span-3 ">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default layout;
