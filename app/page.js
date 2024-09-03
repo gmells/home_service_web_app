d020b9ae029e9bb71c8d9a2d0a2e9addfccc5abf
gmells <victormells30@gmail.com>
2024-09-03 14:09:40 +0100

    Add BusinessList component to main page

--- app/page.js
+++ app/page.js
@@ -5,22 +5,41 @@ import Hero from "./components/Hero";
 import CategoryList from "./components/CategoryList";
 import GlobalApi from "./services/GlobalApi";
 import { useEffect, useState } from "react";
+import BusinessList from "./components/BusinessList";
 
 export default function Home() {
   const [categoryList, setCategoryList] = useState([]);
+  const [businessList, setBusinessList] = useState([]);
   useEffect(() => {
     getCategoryList();
+    getAllBusinessList();
   }, []);
+
+  // Used to get all Category List
+
   const getCategoryList = () => {
     GlobalApi.getCategory().then((resp) => {
       setCategoryList(resp.categories);
     });
   };
+
+  /*
+   *Used to get all Business List
+   */
+
+  const getAllBusinessList = () => {
+    GlobalApi.getAllBusinessList().then((resp) => {
+      setBusinessList(resp.businessLists);
+    });
+  };
+
   return (
     <div>
       <Hero />
 
       <CategoryList categoryList={categoryList} />
+
+      <BusinessList businessList={businessList} title={"Popular Business"} />
     </div>
   );
 }
