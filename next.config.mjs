dbbbda826d2b1bfd5de155dbfdec7c174e248872
gmells <victormells30@gmail.com>
2024-09-03 15:57:44 +0100

    Add the descope website link to fetch authentication page data

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["eu-west-2.graphassets.com"],
+    domains: ["eu-west-2.graphassets.com", "lh3.googleusercontent.com"],
   },
 };
 
