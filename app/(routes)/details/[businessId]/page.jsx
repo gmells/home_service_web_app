d6fdf48872197659de8c55bf21fe37827200937e
gmells <victormells30@gmail.com>
2024-09-04 11:11:51 +0100

    add SuggestedBusinessList and BusinessDescription component

--- app/(routes)/details/[businessId]/page.jsx
+++ app/(routes)/details/[businessId]/page.jsx
@@ -3,6 +3,8 @@ import GlobalApi from "@/app/services/GlobalApi";
 import { signIn, useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import BusinessInfo from "../components/BusinessInfo";
+import SuggestedBusinessList from "../components/SuggestedBusinessList";
+import BusinessDescription from "../components/BusinessDescription";
 
 const BusinessDetail = ({ params }) => {
   const { data, status } = useSession();
@@ -37,6 +39,15 @@ const BusinessDetail = ({ params }) => {
     business && (
       <div className="py-8 md:py-20 px-10 md:px-36">
         <BusinessInfo business={business} />
+
+        <div className="grid grid-cols-3 mt-16">
+          <div className="col-span-3 md:col-span-2 order-last md:order-first">
+            <BusinessDescription business={business} />
+          </div>
+          <div className="">
+            <SuggestedBusinessList business={business} />
+          </div>
+        </div>
       </div>
     )
   );
